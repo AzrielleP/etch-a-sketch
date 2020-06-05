88e27d71bd2245d4fc7d645c6e1ddb4ba02507d4
Azrielle <azriellepelayo@gmail.com>
2020-06-05 20:55:28 +0800

    Added buttons for pen choices

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+let gridNumber = +prompt("What grid size do you want?");
 let container = document.querySelector(".container");
 let clear = document.querySelector(".clear");
-
-let gridNumber = +prompt("What grid size do you want?");
-
+let rainbowPen = document.querySelector(".rainbowPen");
+let pencil = document.querySelector(".pencil");
+let blackPen = document.querySelector(".blackPen");
+let buttons = document.querySelector(".buttonContainer");
 
 createGrid(gridNumber);
-
+startDraw();
 
 function createGrid(gridNumber){
     let div;
@@ -17,26 +19,65 @@ function createGrid(gridNumber){
             container.appendChild(div);
         }
     }
-
-    function clearCanvas(){
-        clear.addEventListener("click", ()=>{
-            let all = document.querySelectorAll(".box");
-            for (let i = 0; i < all.length; i++){
-                all[i].style.backgroundColor = "white";
-            }
-        })
-        
-    }
-    draw();
-    clearCanvas();
 }
 
-function draw(){
-    container.addEventListener("mouseover", event=>{
-        if(event.target !== event.currentTarget){
-        event.target.style.backgroundColor="black";
+function startDraw(){
+    buttons.addEventListener("click", choosePen);
+
+    function choosePen(event){
+        let chosenPen = event.target.className;
+        if (chosenPen == "clear"){
+            clearCanvas();
         }
-        event.stopPropagation();
+        else if (chosenPen == "blackPen"){
+            useBlackPen();
+        }
+        else if(chosenPen == "rainbowPen"){
+            useRainbowPen();
+        }
+        else if(chosenPen == "pencil"){
+            usePencil();
+        }
+    }
+}
+
+function clearCanvas(){
+    clear.addEventListener("click", ()=>{
+        let all = document.querySelectorAll(".box");
+        for (let i = 0; i < all.length; i++){
+            all[i].style.backgroundColor = "white";
+        }
+    }) 
+}
+
+function useRainbowPen(){
+    rainbowPen.addEventListener("click", () => {
+        let hex = "0123456789ABCDEF";
+        let bgColor = "#";
+        for(let i =0; i < 6; i++){
+            bgColor += hex[Math.floor(Math.random() * hex.length)];
+        }
+        draw(bgColor);
     })
 }
 
+function randomizeColor(){
+    container.addEventListener("")
+}
+
+function useBlackPen(){
+    draw("black");
+}
+
+function usePencil(){
+
+}
+
+function draw(pen){
+    container.addEventListener("mouseover", event=>{
+        if(event.target !== event.currentTarget){
+            event.target.style.backgroundColor=pen;
+            }
+            event.stopPropagation();
+    })
+}
